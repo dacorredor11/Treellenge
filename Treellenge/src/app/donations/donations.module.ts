858741c3b34c539e397b7a5a2e11c9be74bd94cf
Steven <steven.brand1999@gmail.com>
2020-05-30 23:10:21 -0500

    [front] Donations

--- Treellenge/src/app/donations/donations.module.ts
+++ Treellenge/src/app/donations/donations.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { DonationsPageRoutingModule } from './donations-routing.module';
 
 import { DonationsPage } from './donations.page';
+import { FooterComponent } from '../component/footer/footer.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { DonationsPage } from './donations.page';
     IonicModule,
     DonationsPageRoutingModule
   ],
-  declarations: [DonationsPage]
+  declarations: [DonationsPage, FooterComponent]
 })
 export class DonationsPageModule {}
